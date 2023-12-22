75adf0104aa82fe10960129d48520dc13e6bd04e
yukdawn <yukdawn@gmail.com>
2023-12-23 00:31:06 +0800

    add: 除了render三个api外，其他均已暴露

--- projects/excalidraw/src/lib/excalidraw.component.ts
+++ projects/excalidraw/src/lib/excalidraw.component.ts
@@ -1,16 +1,19 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {render, createElement} from 'preact';
 import {Excalidraw} from "@excalidraw/excalidraw";
 import {
   AppState,
   BinaryFileData,
   BinaryFiles,
-  ExcalidrawImperativeAPI, ExcalidrawInitialDataState, LibraryItems, LibraryItemsSource,
+  ExcalidrawImperativeAPI, ExcalidrawInitialDataState, Gesture, LibraryItems, LibraryItemsSource,
   PointerDownState, SceneData, SidebarName, ToolType,
+  UIOptions,
 } from '@excalidraw/excalidraw/types/types';
 import App from '@excalidraw/excalidraw/types/components/App';
 import type React from 'react'
-import {ExcalidrawElement, NonDeletedExcalidrawElement} from '@excalidraw/excalidraw/types/element/types';
+import {ExcalidrawElement, NonDeletedExcalidrawElement, Theme} from '@excalidraw/excalidraw/types/element/types';
+import {ClipboardData} from '@excalidraw/excalidraw/types/clipboard';
+import {Language} from '@excalidraw/excalidraw/types/i18n';
 
 @Component({
   selector: 'lib-excalidraw',
@@ -24,8 +27,59 @@ export class ExcalidrawComponent implements OnInit, ExcalidrawImperativeAPI {
   @ViewChild("container", {static: true})
   container!: ElementRef;
   private excalidrawAPI!: ExcalidrawImperativeAPI;
-  initialData: ExcalidrawInitialDataState | null | Promise<ExcalidrawInitialDataState | null> = null;
 
+  @Input()
+  initialData?: ExcalidrawInitialDataState | null | Promise<ExcalidrawInitialDataState | null>;
+  @Input()
+  UIOptions?: Partial<UIOptions>;
+  @Input()
+  isCollaborating?: boolean;
+  @Input()
+  beforePaste?: (data: ClipboardData, event: ClipboardEvent | null) => Promise<boolean> | boolean;
+  @Input()
+  langCode?: Language["code"];
+  @Input()
+  viewModeEnabled?: boolean;
+  @Input()
+  zenModeEnabled?: boolean;
+  @Input()
+  gridModeEnabled?: boolean;
+  @Input()
+  objectsSnapModeEnabled?: boolean;
+  @Input()
+  libraryReturnUrl?: string;
+  @Input()
+  theme?: Theme;
+  @Input()
+  name?: string;
+  @Input()
+  detectScroll?: boolean;
+  @Input()
+  handleKeyboardGlobally?: boolean;
+  @Input()
+  beforeLibraryChange?: (libraryItems: LibraryItems) => void | Promise<any>;
+  @Input()
+  autoFocus?: boolean;
+  @Input()
+  generateIdForFile?: (file: File) => string | Promise<string>;
+  @Input()
+  validateEmbeddable?: boolean | string[] | RegExp | RegExp[] | ((link: string) => boolean | undefined);
+
+  @Output()
+  change = new EventEmitter<{elements: readonly ExcalidrawElement[], appState: AppState, files: BinaryFiles}>();
+  @Output()
+  pointerUpdate = new EventEmitter<{
+    pointer: {
+      x: number;
+      y: number;
+      tool: "pointer" | "laser";
+    };
+    button: "down" | "up";
+    pointersMap: Gesture["pointers"];
+  }>();
+  @Output()
+  pointerDown = new EventEmitter<{activeTool: AppState["activeTool"], pointerDownState: PointerDownState}>();
+  scrollChange = new EventEmitter<{scrollX: number, scrollY: number}>();
   constructor() {
   }
 
@@ -33,37 +87,29 @@ export class ExcalidrawComponent implements OnInit, ExcalidrawImperativeAPI {
     render(createElement(Excalidraw, {
       initialData: this.initialData,
       excalidrawAPI: (api: ExcalidrawImperativeAPI) => this.excalidrawAPI = api,
+      UIOptions: this.UIOptions,
+      onChange: (elements, appState, files) => this.change.emit({elements, appState, files}),
+      isCollaborating: this.isCollaborating,
+      onPointerUpdate: payload => this.pointerUpdate.emit(payload),
+      onPaste: this.beforePaste,
+      langCode: this.langCode,
+      viewModeEnabled: this.viewModeEnabled,
+      zenModeEnabled: this.zenModeEnabled,
+      gridModeEnabled: this.gridModeEnabled,
+      objectsSnapModeEnabled: this.objectsSnapModeEnabled,
+      libraryReturnUrl: this.libraryReturnUrl,
+      theme: this.theme,
+      name: this.name,
+      detectScroll: this.detectScroll,
+      handleKeyboardGlobally: this.handleKeyboardGlobally,
+      onLibraryChange: this.beforeLibraryChange,
+      autoFocus: this.autoFocus,
+      generateIdForFile: this.generateIdForFile,
+      onPointerDown: (activeTool,pointerDownState)=> this.pointerDown.emit({activeTool,pointerDownState}),
+      onScrollChange: (scrollX, scrollY) => this.scrollChange.emit({scrollX,scrollY}),
+      validateEmbeddable: this.validateEmbeddable,
     }), this.container.nativeElement);
   }
-
-
-  data = {
-    elements: [
-      {
-        type: "rectangle",
-        version: 141,
-        versionNonce: 361174001,
-        isDeleted: false,
-        id: "oDVXy8D6rom3H1-LLH2-f",
-        fillStyle: "hachure",
-        strokeWidth: 1,
-        strokeStyle: "solid",
-        roughness: 1,
-        opacity: 100,
-        angle: 0,
-        x: 100.50390625,
-        y: 93.67578125,
-        strokeColor: "#000000",
-        backgroundColor: "transparent",
-        width: 186.47265625,
-        height: 141.9765625,
-        seed: 1968410350,
-        groupIds: [],
-      },
-    ],
-    appState: {zenModeEnabled: true, viewBackgroundColor: "#a5d8ff"},
-    scrollToContent: true,
-  };
 
   addFiles(data: BinaryFileData[]): void {
     return this.excalidrawAPI.addFiles(data);
